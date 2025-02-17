6588da22a7eeff24cae32390163df605656d49fb
TUAN\letha <lethanhtuan900@gmail.com>
2025-02-17 17:35:37 +0700

    FE 17/02

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
  import FileUpload from './components/FileUpload.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
   <div id="app">
       <file-upload></file-upload>
@@ -18,30 +10,5 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
